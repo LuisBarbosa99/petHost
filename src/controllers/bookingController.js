e1593efcccc61bd19cc7314542554ba016b04a05
Luis Barbosa <luiselbarbosa@gmail.com>
2020-08-10 18:43:00 -0300

    function store in 'bookingController' created

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -1,7 +1,44 @@
 const Booking = require('../models/Booking');
+const Pet = require('../models/Pet');
+const Host = require('../models/Host');
 
 module.exports = {
     async store(request,response){
+        const {petId, hostUsername, date, description} = request.body;
+        const {ownerUsername} = request.params;
 
+        try {
+            const owner = await User.findOne({username: ownerUsername});
+
+            if(!owner)
+                return response.status(400).send({error: 'User does not exist'});
+
+            const pet = await Pet.findOne({_id: petId});
+
+            if(!pet)
+                return response.status(400).send({error: 'Pet does not exist'});
+            
+            const host  = await Host.findOne({username: hostUsername});
+
+            if(!host)
+                return response.status(400).send({error: 'Host does not exist'});
+            
+            const booking = await Booking.create({
+                petName: pet.name,
+                petId,
+                hostUsername,
+                ownerUsername,
+                date,
+                description,
+            });
+
+            if(!booking)
+                return response.status(200).send({message: 'OK'});
+    
+
+            return response.status(201).send({message: 'Booking Created Successfully'});
+        } catch (error) {
+            return response.status(400).send({message: 'Booking Registration Failed'});
+        }
     },
 }
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
-const authController = require('./controllers/authController');
+const userController = require('./controllers/userController');
 const petController = require('./controllers/petController');
+const bookingController = require('./controllers/bookingController');
 
 const routes = express.Router();
 
-routes.post('/users', authController.store);
-routes.get('/users', authController.show);
+routes.post('/users', userController.store);
+routes.get('/users', userController.show);
 routes.post('/:ownerUsername/pets', petController.store);
+routes.post('/:ownerUsername/bookings', bookingController.store);
 
 module.exports = routes;
